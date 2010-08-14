cd085e40e4d0321e63702f49346f072eb6b26c08
Jonathan Worthington <jnthn@jnthn.net>
2010-08-15 00:55:00 +0200

    try tweaks.

--- dotnet/compiler/try.bat
+++ dotnet/compiler/try.bat
@@ -1,8 +1,8 @@
 @nmake /NOLOGO
 @parrot compile.pir %1 > x.cs
-@csc x.cs /reference:..\runtime\bin\Debug\RakudoRuntime.dll
 @copy ..\runtime\bin\Debug\RakudoRuntime.dll .
 @copy ..\runtime\bin\Debug\RakudoRuntime.pdb .
+@csc x.cs /reference:RakudoRuntime.dll
 @echo ---
 @x
 

2b52c857bfdb161e09739d3ad5c3767ebb21f464
Jonathan Worthington <jnthn@jnthn.net>
2010-08-16 22:37:11 +0200

    Add some simple signature/parameter classes. We'll just do these as guts-y things for now rather than full blown objects.

---
+++ dotnet/runtime/Runtime/Signatures/Signature.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rakudo.Runtime
+{
+    /// <summary>
+    /// Represents a signature.
+    /// </summary>
+    public class Signature
+    {
+        public Parameter[] Parameters;
+    }
+}

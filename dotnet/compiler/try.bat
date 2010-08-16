eeaea26209e80ecd575887ea3364bef24b0bf15b
Jonathan Worthington <jnthn@jnthn.net>
2010-08-17 01:30:03 +0200

    Start to build NQPSetting.pm into a DLL. We can't make use of it yet, but this gets the build infrastructure in place.

--- dotnet/compiler/try.bat
+++ dotnet/compiler/try.bat
@@ -1,7 +1,7 @@
-@nmake /NOLOGO
-@parrot compile.pir %1 > x.cs
 @copy ..\runtime\bin\Debug\RakudoRuntime.dll .
 @copy ..\runtime\bin\Debug\RakudoRuntime.pdb .
+@nmake /NOLOGO
+@parrot compile.pir %1 > x.cs
 @csc x.cs /reference:RakudoRuntime.dll
 @echo ---
 @x
--- dotnet/compiler/try.sh
+++ dotnet/compiler/try.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # this version of try for use on Linux
-make
-parrot compile.pir $1 > x.cs
 cp ../runtime/bin/Debug/RakudoRuntime.dll .
 cp ../runtime/bin/Debug/RakudoRuntime.pdb .
+make
+parrot compile.pir $1 > x.cs
 gmcs x.cs /reference:RakudoRuntime.dll
 echo ---
 ./x.exe

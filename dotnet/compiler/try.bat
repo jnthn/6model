c20047e0813d814cc261ef5e0366188162ecd10d
Jonathan Worthington <jnthn@jnthn.net>
2010-08-21 00:02:09 +0200

    Make PDB files for the stuff we compile, so it's easier to go debugging.

--- dotnet/compiler/try.bat
+++ dotnet/compiler/try.bat
@@ -2,7 +2,7 @@
 @copy ..\runtime\bin\Debug\RakudoRuntime.pdb .
 @nmake /NOLOGO
 @parrot compile.pir %1 > x.cs
-@csc x.cs /reference:RakudoRuntime.dll
+@csc x.cs /reference:RakudoRuntime.dll /debug
 @echo ---
 @x
 

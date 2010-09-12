1db95fb2319f4bdc7e507fcbe88b6ad1f9b9450c
Jonathan Worthington <jnthn@jnthn.net>
2010-09-12 02:29:46 +0200

    Add a struct containing a bunch of function pointers that map to the representation API (or at least, what we have got defined so far). Again, not checked it compiles yet - just stubbing with notes.

---
+++ parrot/src/metamodel/repr.h
@@ -0,0 +1,65 @@
+/* A representation is what controls the layout of an object and storage of
+ * attributes, as well as how it boxes/unboxes to the three primitive types
+ * INTVAL, FLOATVAL and STRING * (if it can).
+ *
+ * All representations will either use this struct directly or embed it in
+ * their own struct followed by any data they're interested in. Essentially,
+ * it defines the set of functions that a representation should implement to
+ * fulfil the representation API. */
+typedef struct {
+    /* Creates a new type object of this representation, and
+     * associates it with the given HOW. Also sets up a new
+     * representation instance if needed. */
+    PMC * (*type_object_for) (PMC *self, PMC *HOW);
+
+    /* Creates a new instance based on the type object. */
+    PMC * (*instance_of) (PMC *self, PMC *WHAT);
+
+    /* Checks if a given object is defined (from the point of
+     * view of the representation). */
+    INTVAL (*defined) (PMC *self, PMC *Obj);
+
+    /* Gets the current value for an attribute. */
+    PMC * (*get_attribute) (PMC *self, PMC *Object, PMC *ClassHandle, STRING *Name);
+
+    /* Gets the current value for an attribute, obtained using the
+     * given hint.*/
+    PMC * (*get_attribute_with_hint) (PMC *self, PMC *Object, PMC *ClassHandle, STRING *Name, INTVAL Hint);
+
+    /* Binds the given value to the specified attribute. */
+    void (*bind_attribute) (PMC *self, PMC *Object, PMC *ClassHandle, STRING *Name, PMC *Value);
+
+    /* Binds the given value to the specified attribute, using the
+     * given hint. */
+    void (*bind_attribute_with_hint) (PMC *self, PMC *Object, PMC *ClassHandle, STRING *Name, INTVAL Hint, PMC *Value);
+
+    /* Gets the hint for the given attribute ID. */
+    INTVAL (*hint_for) (PMC *self, PMC *ClassHandle, STRING *Name);
+
+    /* Used with boxing. Sets an integer value, for representations that
+     * can hold one. */
+    void (*set_int) (PMC *self, PMC *Object, INTVAL Value);
+
+    /* Used with boxing. Gets an integer value, for representations that
+     * can hold one. */
+    INTVAL (*get_int) (PMC *self, PMC *Object);
+
+    /* Used with boxing. Sets a floating point value, for representations that
+     * can hold one. */
+    void (*set_num) (PMC *self, PMC *Object, FLOATVAL Value);
+
+    /* Used with boxing. Gets a floating point value, for representations that
+     * can hold one. */
+    FLOATVAL (*get_num) (PMC *self, PMC *Object);
+
+    /* Used with boxing. Sets a string value, for representations that
+     * can hold one. */
+    void (*set_str) (PMC *self, PMC *Object, STRING *Value);
+
+    /* Used with boxing. Gets a string value, for representations that
+     * can hold one. */
+    STRING * (*get_str) (PMC *self, PMC *Object);
+
+    /* This Parrot-specific addition to the API is used to mark an object. */
+    void * (*gc_mark) (PMC *self, PMC *Object);
+} Rakudo_Metamodel_Representation;

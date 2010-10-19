88190a82b08d866653c05d02dd1f450f3de3c2e2
Jonathan Worthington <jnthn@jnthn.net>
2010-10-20 01:05:09 +0200

    [common] Fill out NQPHash a bit and...we have basically functioning hashes.

--- common/NQP/NQPSetting.pm
+++ common/NQP/NQPSetting.pm
@@ -108,6 +108,18 @@ knowhow NQPHash is repr('P6mapping') {
     method new() {
         nqp::instance_of(self.WHAT)
     }
+    method Numeric() {
+        self.elems
+    }
+    method elems() {
+        nqp::llmapping_elems(self)
+    }
+    method at_key($key) {
+        nqp::llmapping_get_at_key(self, $key.Str)
+    }
+    method bind_key($key, $value) {
+        nqp::llmapping_bind_at_key(self, $key.Str, $value)
+    }
 }
 
 # This is a little bit of a cheat. We only really need to keep

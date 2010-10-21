47c868ada6acff2fb912be70396aa2e5163e1770
Jonathan Worthington <jnthn@jnthn.net>
2010-10-21 23:29:05 +0200

    [dotnet] Min codegen e kass - make things a bit less noisy.

--- dotnet/compiler/try.bat
+++ dotnet/compiler/try.bat
@@ -2,7 +2,7 @@
 @copy ..\runtime\bin\Debug\RakudoRuntime.pdb .
 @nmake /NOLOGO
 @parrot compile.pir %1 > x.cs
-@csc x.cs /reference:RakudoRuntime.dll /debug
+@csc x.cs /reference:RakudoRuntime.dll /debug /warn:0
 @echo ---
 @x
 

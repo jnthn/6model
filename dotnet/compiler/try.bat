4af6eddfeabaeafe7a47671e7d58bda3cf34a3fb
Jonathan Worthington <jnthn@jnthn.net>
2010-10-28 18:59:09 +0200

    [dotnet] Tweak try.bat to clear up the executable before trying to make a new one, so we don't get bogus test passes. Noticed by mberends++.

--- dotnet/compiler/try.bat
+++ dotnet/compiler/try.bat
@@ -1,5 +1,7 @@
 @copy ..\runtime\bin\Debug\RakudoRuntime.dll .
 @copy ..\runtime\bin\Debug\RakudoRuntime.pdb .
+@del x.exe
+@del x.cs
 @nmake /NOLOGO
 @parrot compile.pir %1 > x.cs
 @csc x.cs /reference:RakudoRuntime.dll /debug /warn:0

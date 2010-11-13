8f4d667f489e8c1a5720731c8e62a86f472275ef
Jonathan Worthington <jnthn@jnthn.net>
2010-11-13 17:38:56 +0100

    [common] Add a multi method Str(Mu:U $self:) { ... } to Mu so type objects can stringify. Finally! \o/

--- common/NQP/P6Objects.pm
+++ common/NQP/P6Objects.pm
@@ -6,4 +6,9 @@ class Mu {
     method CREATE() {
         nqp::instance_of(self)
     }
+
+    proto method Str() { * }
+    multi method Str(Mu:U $self:) {
+        self.HOW.name(self) ~ '()'
+    }
 }
